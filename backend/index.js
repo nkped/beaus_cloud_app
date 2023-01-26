f262546bf57ca55984a42d282c216be8a1b5ad16
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-01-26 17:51:59 +0100

    clean-up commit

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@ import app from "./server.js"
 import mongodb from "mongodb"
 import dotenv from "dotenv"
 
-
 dotenv.config()
+
 const MongoClient = mongodb.MongoClient
 
 const port = process.env.PORT || 8000
